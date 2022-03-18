c8f330eec75b49284ece6195846a66e83d662f9c
axel-gonzalez-dev <axel.gonzalez.dev@gmail.com>
2022-03-18 08:07:58 -0600

    Adding navbar, hero and about page with TailwindCSS framework

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				'asphalt': '#0E0605',
+				'desert': '#ffffff',
+				'jumbo': '#7A7A7C',
+				'friar': '84847D'
+			}
+		},
+		fontFamily:{
+			roboto: ['Roboto', 'sans-serif'],
+		}
+	},
+	plugins: [],
+}
