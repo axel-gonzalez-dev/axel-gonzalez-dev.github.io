109059669c490d11f69af96694d37e6866cdedf2
Axel González <axel.gonzalez.dev@gmail.com>
2023-06-06 13:10:39 -0600

    Move project to root directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {
-			colors: {
-				'asphalt': '#0E0605',
-				'desert': '#ffffff',
-				'jumbo': '#7A7A7C',
-				'friar': '84847D'
-			}
-		},
-		fontFamily:{
-			roboto: ['Roboto', 'sans-serif'],
-		}
-	},
-	plugins: [],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'asphalt': '#0E0605',
+        'desert': '#ffffff',
+        'jumbo': '#7A7A7C',
+        'friar': '84847D'
+      }
+    },
+  },
+  plugins: [],
 }
