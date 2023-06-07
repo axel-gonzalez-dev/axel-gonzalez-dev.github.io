71f276456f173c4a3f14ec595f7be80dcd056889
Axel González <axel.gonzalez.dev@gmail.com>
2023-06-07 10:19:02 -0600

    Add rest of design
    Adding rest of design to site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,107 @@
 import Image from "next/image"
+import Link from "next/link";
 
 /* Import icons */
-import twitterIcon from "../../assets/icons/twitter.svg";
-import linkedinIcon from "../../assets/icons/linkedin.svg";
 import codepenIcon from "../../assets/icons/codepen.svg";
+import leetCode from "../../assets/icons/leetcode.svg";
+import platziIcon from "../../assets/icons/platzi.svg";
+import codecademyIcon from "../../assets/icons/codecademy.svg";
+import udemyIcon from "../../assets/icons/udemy.svg";
+
+/* Import icons as JSX */
+import LinkedinIcon from '../assets/icons/linkedin';
+import TwitterIcon from '../assets/icons/twitter';
 
 export default function Home() {
   return (
     <>
       <main>
 
-        <section id="about" className="px-4 py-16 | bg-asphalt | text-desert text-center">
-          <span>My name is</span>
-          <h1 className="text-4xl font-bold | mt-5">Axel Gonzalez</h1>
-          <button className="mt-7 | bg-desert | text-asphalt font-bold | p-2 | rounded-xl">I’m a frontend
-            developer</button>
-        </section>
-
-        <section className="px-4 py-20 | text-center">
+        <section id="hero" className="bg-desert dark:bg-asphalt | text-asphalt dark:text-desert text-center | min-h-[70vh] | flex justify-center items-center">
           <div>
-            <h2 className="mb-2 | font-medium">Who i am <br></br> <span className="font-bold text-2xl">Hello</span></h2>
-            <p>I am a junior frontend developer. I like to use <b>MERN</b> as my main stack and other CSS libraries
-              like <b>Bootstrap</b> or <b>TailwindCSS</b></p>
+            <h1 className="text-5xl font-bold">Axel Gonzalez</h1>
+            <p className="font-mono text-lg | mt-2">Frontend developer / QA Automation Tester</p>
+            <a href="#about" type="button" className="mt-4 | p-2 | bg-asphalt dark:bg-desert | text-desert dark:text-asphalt hover:bg-gray-700 dark:hover:bg-gray-300 | font-bold | rounded-xl | cursor-pointer">About me</a>
           </div>
         </section>
 
-        <section id="works" className="px-4 py-8 | text-center text-desert | bg-asphalt">
-          <h2 className="text-3xl font-bold">Works</h2>
-          <h3 className="text-lg font-bold">Awesome projects</h3>
+        <section id="about" className="text-center | min-h-[60vh] | bg-gray-50 dark:bg-gray-900 | text-asphalt dark:text-desert | flex justify-center items-center | p-4 py-10">
+          <div>
+            <h2 className="mb-7 | font-bold | text-3xl">About me</h2>
+            <p className="text-lg">I am a Frontend Develope & QA Automation with. Loves building responsive web apps & automating tests with React & Cypress JS</p>
 
-          <div className="mt-5">
-            <p className="animate-bounce">I am still working on create awesome projects, come later to see more 👀</p>
+            <div className="mt-20 | flex justify-around">
+              <div>
+                <h5 className="font-bold text-lg">Frontend</h5>
+
+                <ul className="mt-5">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Javascript</li>
+                  <li>React JS</li>
+                  <li>Bootstrap CSS</li>
+                  <li>Tailwind CSS</li>
+                </ul>
+              </div>
+
+              <div>
+                <h5 className="font-bold text-lg">QA Automation Tester</h5>
+
+                <ul className="mt-5">
+                  <li>Cypress JS</li>
+                  <li>Mocha JS</li>
+                  <li>Jest</li>
+                </ul>
+              </div>
+
+              <div>
+                <h5 className="font-bold text-lg">Other skills</h5>
+
+                <ul className="mt-5">
+                  <li>AWS API Gateway</li>
+                  <li>AWS Cognito</li>
+                  <li>Git</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </section>
 
-        <section id="contact" className="px-4 py-12 | text-center">
-          <h2 className="text-3xl font-bold | mb-6">Contact</h2>
-          <div className="flex | justify-around">
-            <a href="https://www.linkedin.com/in/davidaxel121/" target="_blank">
-              <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300" priority src={linkedinIcon} alt="Twitter icon" />
-            </a>
-            <a href="https://twitter.com/axelgonzalezdev" target="_blank">
-              <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300" priority src={twitterIcon} alt="Twitter icon" />
-            </a>
-            <a href="https://codepen.io/axel-gonzalez-dev" target="_blank">
-              <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300" priority src={codepenIcon} alt="Twitter icon" />
-            </a>
+        <section id="projects" className="text-center | flex justify-center items-center | min-h-[50vh] | bg-desert dark:bg-asphalt | text-asphalt dark:text-desert | p-4">
+          <div>
+            <h2 className="mb-7 | font-bold | text-3xl">Projects</h2>
+
+            <p className="animate-bounce">I am still working on create some awesome projects, come later to see more 👀</p>
+          </div>
+        </section>
+
+        <section id="contact" className="text-center | flex justify-center items-center | min-h-[40vh] | bg-gray-50 dark:bg-gray-900 | text-asphalt dark:text-desert | p-4 py-10">
+          <div className="min-w-full">
+            <h2 className="text-3xl font-bold | mb-20">Contact</h2>
+            
+            <div className="grid grid-cols-3 gap-8 place-items-center">
+              <Link href="https://www.linkedin.com/in/axel-gonzalez-dev/" target="_blank" className="animate-pulse hover:animate-none ease-in duration-300 | fill-asphalt dark:fill-desert">
+                <LinkedinIcon/>
+              </Link>
+              <Link href="https://twitter.com/axelgonzalezdev" target="_blank" className="animate-pulse hover:animate-none ease-in duration-300 | fill-asphalt dark:fill-desert">
+                <TwitterIcon/>
+              </Link>
+              <Link href="https://codepen.io/axel-gonzalez-dev" target="_blank">
+                <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300 hover:fill-blue-700" src={codepenIcon} alt="Codepen icon" />
+              </Link>
+              <Link href="https://leetcode.com/axelgonzalezdev/" target="_blank">
+                <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300 hover:fill-blue-700" src={leetCode} alt="Leetcode icon" />
+              </Link>
+              <Link href="https://platzi.com/p/davidaxel121/" target="_blank">
+                <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300 hover:fill-blue-700" src={platziIcon} alt="Platzi icon" />
+              </Link>
+              <Link href="https://www.codecademy.com/profiles/axel.gonzalez.dev" target="_blank">
+                <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300 hover:fill-blue-700" src={codecademyIcon} alt="Codecademy icon" />
+              </Link>
+              <Link href="https://www.udemy.com/user/david-axel-gonzalez-flores/" target="_blank">
+                <Image width={36} height={40} className="animate-pulse hover:animate-none ease-in duration-300 hover:fill-blue-700" src={udemyIcon} alt="Udemy icon" />
+              </Link>
+            </div>
           </div>
         </section>
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -43,21 +43,21 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="bg-desert text-asphalt | dark:bg-asphalt dark:text-desert | sticky top-0 z-10">
+            <nav className="bg-desert text-asphalt | dark:bg-asphalt dark:text-desert | fixed top-0 z-10 | w-full">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link href='/' className="text-2xl font-semibold whitespace-nowrap | hover:text-blue-700">
                         Axel Gonzalez
                     </Link>
 
-                    <button className="p-2 | md:hidden | hover:bg-blue-700" onClick={toggleNavbar}>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    <button className="p-2 | md:hidden | hover:bg-blue-600" onClick={toggleNavbar}>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
 
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul className="font-medium | flex flex-col md:flex-row md:space-x-8 | mt-4 md:mt-0">
                             {
-                                NavbarLinks.map(({ name, url }: any) => (
-                                    <li>
+                                NavbarLinks.map(({ name, url }: any, index) => (
+                                    <li key={index}>
                                         <Link href={url} className="block | py-2 pl-3 md:p-0 | bg-desert dark:bg-asphalt | text-asphalt dark:text-desert hover:text-inherit md:hover:text-blue-700 | hover:bg-blue-700 dark:hover:bg-blue-700 md:hover:bg-inherit dark:md:hover:bg-inherit">{name}</Link>
                                     </li>
                                 ))
